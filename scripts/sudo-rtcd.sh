20141df852ef4ffb9382bf259a53791fe4145659
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2022-02-11 12:27:32 +0900

    add sudo-rtcd

---
+++ scripts/sudo-rtcd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+# roslaunchのlaunch-prefixでsudoを使うと、そのノードはctrl-C時に終了しなくなる. これを、ctrl-C時にちゃんと終了するようにする. sudoのパスワードは不要になっている前提
+# ./sudo-rtcd.sh rtcd rtmlaunch hrpsys_tools hrpsys.launch RTCD_LAUNCH_PREFIX:="sudo env -i LANG=C ORBgiopMaxMsgSize=2147483648 PATH=$(env PATH) LD_LIBRARY_PATH=$(env LD_LIBRARY_PATH)
+# rtcd is ROS nodename
+
+RTCDNAME=$1
+ARGS=${@:2}
+
+function kill_rtcd() {
+    echo sending SIGKILL to rtcd...
+    ps aux | grep "__name:=$RTCDNAME " | grep -v grep | awk '{ print "kill -9", $2 }' | sudo sh
+
+    #sudo killall rtmlaunch
+    return 0
+}
+
+trap "echo signal trapped.;kill_rtcd; exit" 1 2 3 15
+
+$ARGS
+
+kill_rtcd
