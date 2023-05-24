2cb9a364fa3de4ce0f90fd81a8b71a4c6677efc9
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2023-05-24 14:28:03 +0900

    add document

--- scripts/sudo-rtcd.sh
+++ scripts/sudo-rtcd.sh
@@ -5,6 +5,12 @@
 # ./sudo-rtcd.sh rtcd rtmlaunch hrpsys_tools hrpsys.launch RTCD_LAUNCH_PREFIX:="sudo env -i LANG=C ORBgiopMaxMsgSize=2147483648 PATH=$(env PATH) LD_LIBRARY_PATH=$(env LD_LIBRARY_PATH)
 # rtcd is ROS nodename
 
+# sudoのパスワードを不要にする方法
+# sudo visudo
+# で
+# <username> ALL=NOPASSWD: ALL
+# を末尾に書く
+
 RTCDNAME=$1
 ARGS=${@:2}
 
